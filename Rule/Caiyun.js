3bd78c1f38139cae8c53de9b60316a8f4377f0be
枭木iOS资源 <74090998+emuiavip@users.noreply.github.com>
2023-08-02 17:25:13 +0800

    Update Caiyun.js

--- Rule/Caiyun.js
+++ Rule/Caiyun.js
@@ -18,34 +18,34 @@ hostname = *.cyapi.cn, adx.sogaha.cn
 *************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
+var tmxios = JSON.parse($response.body);
 const vip = '/vip_info';
 const adhf = '/activity';
 const user = '(user|visitors)';
 
 if ($request.url.indexOf(vip) != -1){
-    chxm1023.vip = {
+    tmxios.vip = {
         "expires_time" : "4092599349",
         "is_auto_renewal" : true
       };
-    chxm1023.svip = {
+    tmxios.svip = {
         "expires_time" : "4092599349",
         "is_auto_renewal" : true
       };
 }
 
 if ($request.url.indexOf(adhf) != -1){
-    chxm1023.activities = [];
+    tmxios.activities = [];
 }
 
 if ($request.url.indexOf('user') != -1){
-    chxm1023.result.ranking_above = 99;
-    chxm1023.result.is_vip = true;
-    chxm1023.result.vip_expired_at = 4092599349;
-    chxm1023.result.svip_given = 9999;
-    chxm1023.result.is_xy_vip = true;
-    chxm1023.result.xy_svip_expire = 4092599349; 
-    chxm1023.result.wt.vip = {
+    tmxios.result.ranking_above = 99;
+    tmxios.result.is_vip = true;
+    tmxios.result.vip_expired_at = 4092599349;
+    tmxios.result.svip_given = 9999;
+    tmxios.result.is_xy_vip = true;
+    tmxios.result.xy_svip_expire = 4092599349; 
+    tmxios.result.wt.vip = {
         "auto_renewal_type" : "",
         "expired_at" : 0,
         "enabled" : true,
@@ -54,26 +54,26 @@ if ($request.url.indexOf('user') != -1){
         "svip_expired_at" : 4092599349,
         "svip_auto_renewal_type" : ""
       };
-    chxm1023.result.wt.svip_given = 9999;
-    chxm1023.result.wt.ranking_above = 99;
-    chxm1023.result.is_phone_verified = true;
-    chxm1023.result.name = "枭木iOS大本营";
-    chxm1023.result.avatar = "https://pan.tmxios.top/d/%E8%BD%AF%E4%BB%B6%E6%BA%90/%E5%8D%9A%E5%AE%A2%E5%9B%BE/logo.png";
-    chxm1023.result.phone_num = "13145200000";
-    chxm1023.result.vip_take_effect = 1;
-    chxm1023.result.is_primary = true;
-    chxm1023.result.xy_vip_expire = 4092599349;
-    chxm1023.result.svip_expired_at = 4092599349;
-    chxm1023.result.svip_take_effect = 1;
-    chxm1023.result.vip_type = "s";
-    chxm1023.result.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
-    chxm1023.result.bound_status.qq.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.weixin.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.caiyun.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.twitter.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.facebook.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.apple.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.weibo.username = "https://t.me/Tmxios";
+    tmxios.result.wt.svip_given = 9999;
+    tmxios.result.wt.ranking_above = 99;
+    tmxios.result.is_phone_verified = true;
+    tmxios.result.name = "枭木iOS大本营";
+    tmxios.result.avatar = "https://pan.tmxios.top/d/%E8%BD%AF%E4%BB%B6%E6%BA%90/%E5%8D%9A%E5%AE%A2%E5%9B%BE/logo.png";
+    tmxios.result.phone_num = "13145200000";
+    tmxios.result.vip_take_effect = 1;
+    tmxios.result.is_primary = true;
+    tmxios.result.xy_vip_expire = 4092599349;
+    tmxios.result.svip_expired_at = 4092599349;
+    tmxios.result.svip_take_effect = 1;
+    tmxios.result.vip_type = "s";
+    tmxios.result.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
+    tmxios.result.bound_status.qq.username = "https://t.me/Tmxios";
+    tmxios.result.bound_status.weixin.username = "https://t.me/Tmxios";
+    tmxios.result.bound_status.caiyun.username = "https://t.me/Tmxios";
+    tmxios.result.bound_status.twitter.username = "https://t.me/Tmxios";
+    tmxios.result.bound_status.facebook.username = "https://t.me/Tmxios";
+    tmxios.result.bound_status.apple.username = "https://t.me/Tmxios";
+    tmxios.result.bound_status.weibo.username = "https://t.me/Tmxios";
 }
 
-$done({ body: JSON.stringify(chxm1023)});
+$done({ body: JSON.stringify(tmxios)});
