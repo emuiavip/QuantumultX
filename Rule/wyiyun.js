d7c499296c1a752c14b67835f0572cf6c340837a
枭木iOS资源 <74090998+emuiavip@users.noreply.github.com>
2023-08-10 13:16:40 +0800

    Update wyiyun.js

--- Rule/wyiyun.js
+++ Rule/wyiyun.js
@@ -1,8 +1,6 @@
-/*
- *
- *
+/***********************************
 
-> 规则名称：阿里云盘净化+解锁本地会员功能[倍速、码率]
+> 规则名称：网易云解锁VIP试听
 > QQ群：681835088
 > 公众号：枭木iOS大本营
 > TG交流群：https://t.me/Tmxios 
@@ -13,9 +11,11 @@
 > 游戏源地址：https://game.tmxios.top/appstore
 > 证书购买或定制：https://p12.tmxios.top
 > 购买卡密地址：https://pay.tmxios.top
-> 更新时间：2023-08-05
+> 更新时间：2023-08-10
 > 转载说明：如需引用请注明出处，谢谢合作！
 *******************************
+
+
 [rewrite_local]
 # > 网易云解锁VIP
 ^http[s]?:\/\/.+music.+(player\/url|playlist|entrance|\/eapi\/search\/).*$ url script-request-header https://raw.githubusercontent.com/WeiRen0/Scripts/main/wyun.js
