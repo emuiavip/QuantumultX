48ee0cc2119889fc517c02813e986be36c3c22ae
枭木iOS资源 <74090998+emuiavip@users.noreply.github.com>
2023-08-12 15:36:49 +0800

    Update Caiyun.js

--- Rule/Caiyun.js
+++ Rule/Caiyun.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/(biz|wrapper)\.cyapi\.cn\/(.+\/(user.+|visitors|activity)|p\/v\d\/vip_info) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyun.js
+^https?:\/\/(biz|wrapper)\.cyapi\.cn\/(.+\/(user.+|visitors|activity)|p\/v\d\/vip_info) url script-response-body https://raw.githubusercontent.com/emuiavip/QuantumultX/main/Rule/Caiyun.js
 ^http:\/\/adx\.sogaha\.cn\/sdk\/ad\/get url reject-200
 
 [mitm]
@@ -59,7 +59,7 @@ if ($request.url.indexOf('user') != -1){
     chxm1023.result.is_phone_verified = true;
     chxm1023.result.name = "枭木iOS大本营";
     chxm1023.result.avatar = "https://pan.tmxios.top/d/%E8%BD%AF%E4%BB%B6%E6%BA%90/%E5%8D%9A%E5%AE%A2%E5%9B%BE/logo.png";
-    chxm1023.result.phone_num = "13145200000";
+    chxm1023.result.phone_num = "13800000000";
     chxm1023.result.vip_take_effect = 1;
     chxm1023.result.is_primary = true;
     chxm1023.result.xy_vip_expire = 4092599349;
@@ -67,13 +67,13 @@ if ($request.url.indexOf('user') != -1){
     chxm1023.result.svip_take_effect = 1;
     chxm1023.result.vip_type = "s";
     chxm1023.result.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
-    chxm1023.result.bound_status.qq.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.weixin.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.caiyun.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.twitter.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.facebook.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.apple.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.weibo.username = "https://t.me/chxm1023";
+    chxm1023.result.bound_status.qq.username = "https://t.me/Tmxios";
+    chxm1023.result.bound_status.weixin.username = "https://t.me/Tmxios";
+    chxm1023.result.bound_status.caiyun.username = "https://t.me/Tmxios";
+    chxm1023.result.bound_status.twitter.username = "https://t.me/Tmxios";
+    chxm1023.result.bound_status.facebook.username = "https://t.me/Tmxios";
+    chxm1023.result.bound_status.apple.username = "https://t.me/Tmxios";
+    chxm1023.result.bound_status.weibo.username = "https://t.me/Tmxios";
 }
 
 $done({ body: JSON.stringify(chxm1023)});
