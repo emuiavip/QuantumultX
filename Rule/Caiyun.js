b4daa3c044d1c54978d7eb69fdcfbe1b66077247
枭木iOS资源 <74090998+emuiavip@users.noreply.github.com>
2023-12-21 11:13:05 +0800

    Update Caiyun.js

--- Rule/Caiyun.js
+++ Rule/Caiyun.js
@@ -18,62 +18,66 @@ hostname = *.cyapi.cn, adx.sogaha.cn
 *************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
-const vip = '/vip_info';
-const adhf = '/activity';
-const user = '(user|visitors)';
+var tmxios = JSON.parse($response.body);
+const urls = ['/vip_info', '/activity', 'user'];
 
-if ($request.url.indexOf(vip) != -1){
-    chxm1023.vip = {
-        "expires_time" : "4092599349",
-        "is_auto_renewal" : true
-      };
-    chxm1023.svip = {
-        "expires_time" : "4092599349",
-        "is_auto_renewal" : true
-      };
-}
+urls.forEach(url => {
+  if ($request.url.includes(url)){
+    switch(url) {
+      case '/vip_info':
+        tmxios = {
+          ...tmxios, 
+          vip: {
+            "expires_time" : "4092599349",
+            "is_auto_renewal" : true
+          },
+          svip: {
+            "expires_time" : "4092599349",
+            "is_auto_renewal" : true
+          }
+        }
+        break;
+      case '/activity':
+        tmxios.activities = [];
+        break;
+      case 'user':
+        tmxios.result = {
+          ...tmxios.result,
+          ranking_above: 99,
+          is_vip: true,
+          vip_expired_at: 4092599349,
+          svip_given: 9999,
+          is_xy_vip: true,
+          xy_svip_expire: 4092599349,
+          wt: {
+            ...tmxios.result.wt,
+            vip: {
+              "auto_renewal_type" : "",
+              "expired_at" : 0,
+              "enabled" : true,
+              "svip_apple_expired_at" : 4092599349,
+              "is_auto_renewal" : true,
+              "svip_expired_at" : 4092599349,
+              "svip_auto_renewal_type" : ""
+            },
+            svip_given: 9999,
+            ranking_above: 99,
+          },
+          is_phone_verified: true,
+          phone_num: "13145200000",
+          vip_take_effect: 1,
+          is_primary: true,
+          xy_vip_expire: 4092599349,
+          svip_expired_at: 4092599349,
+          svip_take_effect: 1,
+          vip_type: "s",
+          name: "枭木iOS大本营",
+          avatar: "https://pan.tmxios.top/d/%E8%BD%AF%E4%BB%B6%E6%BA%90/%E5%8D%9A%E5%AE%A2%E5%9B%BE/logo.png",
+          token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps"
+        }
+        break;
+    }
+  }
+})
 
-if ($request.url.indexOf(adhf) != -1){
-    chxm1023.activities = [];
-}
-
-if ($request.url.indexOf('user') != -1){
-    chxm1023.result.ranking_above = 99;
-    chxm1023.result.is_vip = true;
-    chxm1023.result.vip_expired_at = 4092599349;
-    chxm1023.result.svip_given = 9999;
-    chxm1023.result.is_xy_vip = true;
-    chxm1023.result.xy_svip_expire = 4092599349; 
-    chxm1023.result.wt.vip = {
-        "auto_renewal_type" : "",
-        "expired_at" : 0,
-        "enabled" : true,
-        "svip_apple_expired_at" : 4092599349,
-        "is_auto_renewal" : true,
-        "svip_expired_at" : 4092599349,
-        "svip_auto_renewal_type" : ""
-      };
-    chxm1023.result.wt.svip_given = 9999;
-    chxm1023.result.wt.ranking_above = 99;
-    chxm1023.result.is_phone_verified = true;
-    chxm1023.result.name = "枭木iOS大本营";
-    chxm1023.result.avatar = "https://pan.tmxios.top/d/%E8%BD%AF%E4%BB%B6%E6%BA%90/%E5%8D%9A%E5%AE%A2%E5%9B%BE/logo.png";
-    chxm1023.result.phone_num = "13800000000";
-    chxm1023.result.vip_take_effect = 1;
-    chxm1023.result.is_primary = true;
-    chxm1023.result.xy_vip_expire = 4092599349;
-    chxm1023.result.svip_expired_at = 4092599349;
-    chxm1023.result.svip_take_effect = 1;
-    chxm1023.result.vip_type = "s";
-    chxm1023.result.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
-    chxm1023.result.bound_status.qq.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.weixin.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.caiyun.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.twitter.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.facebook.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.apple.username = "https://t.me/Tmxios";
-    chxm1023.result.bound_status.weibo.username = "https://t.me/Tmxios";
-}
-
-$done({ body: JSON.stringify(chxm1023)});
+$done({ body: JSON.stringify(tmxios)});
