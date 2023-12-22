e8363a4ece2bedf6c24b574311c80ecfa489d16f
枭木iOS资源 <74090998+emuiavip@users.noreply.github.com>
2023-12-22 10:14:39 +0800

    Update bodian.js

--- Rule/bodian.js
+++ Rule/bodian.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/bd-api\.kuwo\.cn\/api\/(ucenter\/users|play\/listening\/user|service\/(home\/index|banner\/myPage|advert\/watch)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/bodian.js
+^https:\/\/bd-api\.kuwo\.cn\/api\/(ucenter\/users|play\/listening\/user|service\/(home\/index|banner\/myPage|advert\/watch)) url script-response-body https://raw.githubusercontent.com/emuiavip/QuantumultX/main/Rule/bodian.js
 
 [mitm]
 hostname = bd-api.kuwo.cn
@@ -18,28 +18,28 @@ hostname = bd-api.kuwo.cn
 
 
 var body = $response.body;
-var chxm1023 = JSON.parse(body);
+var tmxios = JSON.parse(body);
 const vipa = /api\/(ucenter\/users|play\/listening\/user)/;
 const ad = /api\/service\/(home\/index|banner\/myPage)/;
 const advert = /api\/service\/advert\/watch/;
 
 if(vipa.test($request.url)){
-  chxm1023.data.payInfo = {
+  tmxios.data.payInfo = {
       "expireDate" : 4092599349000,
       "isSigned" : 1,
       "lastPayType" : 1,
       "isVip" : 1
     };
-  chxm1023.data.payRights = {
+  tmxios.data.payRights = {
       "headPendant" : {
         "id" : 11,
         "name" : "音波",
         "pic" : "https://bodiancdn.kuwo.cn/file/bc92ceb2fb555e34246cdf4f558015ec.gif"
       }
     };
-  chxm1023.data.isBind = 1;
-  chxm1023.data.userInfo = {
-   ...chxm1023.data.userInfo,
+  tmxios.data.isBind = 1;
+  tmxios.data.userInfo = {
+   ...tmxios.data.userInfo,
    "isVip" : 1,
    "authType" : 3,
    "headOuterImg" : "https://bodiancdn.kuwo.cn/file/bc92ceb2fb555e34246cdf4f558015ec.gif",
@@ -48,18 +48,18 @@ if(vipa.test($request.url)){
 }
 
 if(ad.test($request.url)){
-  chxm1023.data.bannerList = [];
-  if (chxm1023.data.moduleList && chxm1023.data.moduleList.length > 0) {
-   chxm1023.data.moduleList = chxm1023.data.moduleList.filter(item => item.name !== "轮播图" && item.name !== "波点实验室");
+  tmxios.data.bannerList = [];
+  if (tmxios.data.moduleList && tmxios.data.moduleList.length > 0) {
+   tmxios.data.moduleList = tmxios.data.moduleList.filter(item => item.name !== "轮播图" && item.name !== "波点实验室");
   };
 }
 
 if(advert.test($request.url)){
-  chxm1023.data = {
+  tmxios.data = {
     "mvGuide" : "看广告，解锁所有VIP歌曲\n解锁后可畅听%s",
     "expireTime" : 0,
     "mvDuration" : 0
   };
 }
 
-$done({body : JSON.stringify(chxm1023)});
+$done({body : JSON.stringify(tmxios)});
