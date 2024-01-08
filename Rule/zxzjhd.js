c5485ed033c3864cb5610ccfd313d883ce0e6699
枭木iOS资源 <74090998+emuiavip@users.noreply.github.com>
2024-01-08 09:28:56 +0800

    Update zxzjhd.js

--- Rule/zxzjhd.js
+++ Rule/zxzjhd.js
@@ -1,18 +1,12 @@
-/******************************
-> 规则名称：阿里云盘净化+解锁本地会员功能[倍速、码率]
-> QQ群：681835088
-> 公众号：枭木iOS大本营
-> TG交流群：https://t.me/Tmxios 
-> TG频道：https://t.me/Msskymu
-> 枭木官方站：https://tmxios.top
-> 手机卡申请：https://10.tmxios.top
-> 软件源地址：https://app.tmxios.top/appstore
-> 游戏源地址：https://game.tmxios.top/appstore
-> 证书购买或定制：https://p12.tmxios.top
-> 购买卡密地址：https://pay.tmxios.top
-> 更新时间：2023-08-05
-> 转载说明：如需引用请注明出处，谢谢合作！
-***********************************/
+/*************************************
+
+项目名称：在线之家-净化/去广告
+下载地址：https://t.me/Tmxios
+脚本作者：枭木
+电报频道：https://t.me/Tmxios
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
 
 
 [rewrite_local]
