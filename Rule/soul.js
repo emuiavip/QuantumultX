640b5058f338f71e992807448b8a71f6e2d626e2
枭木iOS资源 <74090998+emuiavip@users.noreply.github.com>
2024-06-06 17:28:20 +0800

    Update soul.js

--- Rule/soul.js
+++ Rule/soul.js
@@ -4,6 +4,67 @@
 #!update = 2024-06-06 13:42
 
 
+const url = $request.url;
+const scriptEnvironment = typeof $task !== 'undefined' ? 'Surge' : (typeof $loon !== 'undefined' ? 'Loon' : (typeof $httpClient !== 'undefined' ? 'Qx' : 'Unknown'));
+
+if (!$response.body || scriptEnvironment === 'Unknown') {
+  $done({});
+}
+
+let obj = JSON.parse($response.body);
+
+const shouldDeleteData = (url) => {
+  return url.includes("/post/homepage/guide/card") ||
+         url.includes("/furion/position/content") ||
+         url.includes("/hot/soul/rank") ||
+         url.includes("/post/gift/list") ||
+         url.includes("/mobile/app/version/queryIos") ||
+         url.includes("/teenager/config") ||
+         url.includes("/winterfell/v2/getIpByDomain") || 
+         url.includes("/official/scene/module");
+};
+
+const shouldModifyLimitInfo = (url, obj) => {
+  return url.includes("/chat/limitInfo") && obj.data && obj.data.limit !== undefined;
+};
+
+if (url.includes("/vip/meet/userInfo")) {
+  if (obj.data.superStarDTO && obj.data.superStarDTO.superVIP !== undefined) {
+    obj.data.superStarDTO.superVIP = true;
+    obj.data.superStarDTO.validTime = 9887893999000;
+    obj.data.flyPackageDTO.hasFlyPackage = true;
+  }
+}
+
+if (url.includes("/privilege/supervip/status")) {
+  if (obj.data.superVIP !== undefined) {
+    obj.data.superVIP = true;
+    obj.data.remainDay = 9887893999000;
+    obj.data.hasCancelVIPSubscription = false;
+    obj.data.hasCancelVIPSubOfIAP = false;
+    obj.data.hasFlyPackage = true;
+  }
+}
+
+if (!obj.data || shouldDeleteData(url)) {
+  delete obj.data;
+}
+
+if (shouldModifyLimitInfo(url, obj)) {
+  obj.data.limit = false;
+}
+
+if (url.includes("/post/recSquare/subTabs")) {
+  obj.data = obj.data.filter(item => [7, 6, 2].includes(item.tabType));
+}
+
+$done({ body: JSON.stringify(obj) });
+
+
+
+
+
+  
 ^https:\/\/api-a\.soulapp\.cn\/v2\/post\/gift\/list url script-response-body https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/Soul.js
 ^https:\/\/post\.soulapp\.cn\/hot\/soul\/rank url script-response-body https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/Soul.js
 ^https:\/\/gateway-mobile-gray\.soulapp\.cn\/mobile\/app\/version\/queryIos url script-response-body https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/Soul.js
